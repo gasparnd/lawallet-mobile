3b8f9dd9f87395d2c990f28388f3656c91f535f7
gasparnd <gaspardolcemascolo@gmail.com>
2023-12-13 07:56:13 -0300

    add internalization

--- app/components/HomeHeaderLeft.tsx
+++ app/components/HomeHeaderLeft.tsx
@@ -1,29 +1,36 @@
 import React from 'react';
 import {View} from 'react-native';
+import {useTranslation} from 'react-i18next';
 
+import {useUser} from 'context';
+import {useColors} from 'hooks';
 import {WALLET_DOMAIN} from '@env';
 import CustomText from '../ui/CustomText';
 import Divider from '../ui/Divider';
 import Row from '../ui/Row';
-import {useUser} from 'context';
-import {useColors} from 'hooks';
 import Avatar from './Avatar';
 
 export default function HomeHeaderLeft() {
   const {user} = useUser();
   const {colors} = useColors();
+  const {t} = useTranslation();
   return (
     <View>
       <Row alignItems="center">
         <Avatar name={user.username} />
         <Divider x={4} />
         <View>
-          <CustomText color={colors.gray50} size="small" scaling={false}>
-            Hola,
-          </CustomText>
-          <CustomText size="small" scaling={false}>
-            {`${user.username}@${WALLET_DOMAIN}`}
-          </CustomText>
+          <CustomText
+            txt={`${t('HELLO')},`}
+            color={colors.gray50}
+            size="small"
+            scaling={false}
+          />
+          <CustomText
+            txt={`${user.username}@${WALLET_DOMAIN}`}
+            size="small"
+            scaling={false}
+          />
         </View>
       </Row>
     </View>
